0e1ed025e32071ae47fb06b48a0074a9b745f725
MohamedMurshalIbrahim <mohamedtechcreation@gmail.com>
2025-09-25 10:27:11 +0530

    fix

--- heritage/vite.config.js
+++ heritage/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ancestrar",
+  base: process.env.NODE_ENV || "/react-vite-deploy",
   build: {
     rollupOptions: {
       output: {
